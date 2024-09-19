066d6e8d2b7b844e0f20d9a940ef734900cfff50
hariharan888 <hariharan@drylogics.com>
2024-09-19 11:01:21 +0530

    Add discard feature
    - add gem
    - add column and feature to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
+  include Discard::Model
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -8,4 +9,8 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   validates :name, presence: true
+
+  def active_for_authentication?
+    super && !discarded?
+  end
 end
